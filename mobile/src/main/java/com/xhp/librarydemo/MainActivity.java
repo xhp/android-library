23ef8902f3b4f2893d4ae4518d334cf8f1c81d87
xinhui.peng <jlchome@sina.cn>
2017-12-04 10:44:57 +0800

    init mobile tv

---
+++ mobile/src/main/java/com/xhp/librarydemo/MainActivity.java
@@ -0,0 +1,13 @@
+package com.xhp.librarydemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
